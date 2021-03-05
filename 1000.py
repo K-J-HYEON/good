320f0d6f8f01dda361fd8fc7a7c63bc15bc9ca60
K-J-HYEON <kjh950601@gmail.com>
2021-03-05 17:46:22 +0900

    abc

---
+++ 1000.py
@@ -0,0 +1,14 @@
+#1
+# a,b = input().split()
+# a = int(a)
+# b = int(b)
+
+# print(a+b)
+
+#2
+# a, b = map(int, input().split())
+# print(a+b)
+
+#3
+A, B = input().split() # 입력되는 문자를 input()함수로 입력받고 split()함수로 나누어 A,B 변수에 저장
+print(int(A)+int(B))	# int() 함수로 A와 B를 정수로 변환 하고 두수의 합을 출력
